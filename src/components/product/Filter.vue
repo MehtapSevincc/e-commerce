aa5161c93b2d08d984a621b07420971ab69c072b
MehtapSevincc <sevincmehtap614@gmail.com>
2025-09-05 16:04:27 +0300

    add sort by

---
+++ src/components/product/Filter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-col gap-2">
+        <label class="flex items-center gap-2">
+            <input type="radio"  name="sort" value="price-asc" v-model="localSortOption" />
+            Price:Low to High
+        </label>
+        <label>
+            <input type="radio" name="sort" value="price-desc" v-model="localSortOption" />
+            Price:High to Low
+        </label>
+         <label class="flex items-center gap-2">
+            <input type="radio"  name="sort" value="newest" v-model="localSortOption" />
+           New to Old
+        </label>
+         <label class="flex items-center gap-2">
+            <input type="radio" name="sort" value="oldest" v-model="localSortOption" />
+            Old to New
+        </label>
+    </div>
+</template>
+
+<script setup>
+import { defineEmits,ref,watch } from 'vue';
+const emit =defineEmits(['update:sort']);
+const localSortOption = ref('');
+watch (localSortOption,(newVal)=> {
+    emit('update:sort',newVal)
+});
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,15 @@
 <template>
+  <div class="flex">
+      <aside class="w-1/4 p-4 border-r">
+<Filter @update:sort="handleSearch"/>
+</aside>
+    <main class ="w-3/4">
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
   <ProductCard 
   v-for="product in paginatedProducts"
   :key="product.id"
   :product="product" />
   </div>
-
 
 
     <div class="flex justify-center mt-4 gap-2">
@@ -36,31 +40,57 @@
     </button>
   </div>
 
-
+</main>
+  </div>
 </template>
 
 <script setup>
 import {ref,computed, onMounted } from 'vue';
 import { fetchProducts } from '../stores/product';
 import ProductCard from '../components/product/ProductCard.vue';
+import Filter from '../components/product/Filter.vue';
 
-const allProducts =ref([])
-const currentPage =ref(1)
-const perPage =12
+const allProducts =ref([]);
+const currentPage =ref(1);
+const perPage =12;
+const sortOption =ref('');
+function handleSearch(val){
+  sortOption.value =val;
+  currentPage.value =1;
+}
 
 onMounted (async () => {
   const data =await fetchProducts()
-  console.log('Fetched products:',data)
+    console.log('type of numPrice:', typeof data[0]?.numPrice);
   allProducts.value = data
 })
+const sortedProducts =computed (()=>{
+ const productsCopy = allProducts.value.slice();
+ if(sortOption.value === 'price-asc') {
+  return productsCopy.sort((a,b)=>a.numPrice-b.numPrice);
+ }
+  else if (sortOption.value === 'price-desc') {
+    return productsCopy.sort((a, b) => b.numPrice - a.numPrice);
+  }
+   else if (sortOption.value === 'newest') {
+    return productsCopy.sort((a, b) =>new Date(b.createdAt) - new Date(a.createdAt));
+  }
+   else if (sortOption.value === 'oldest') {
+    return productsCopy.sort((a, b) =>new Date(a.createdAt) - new Date(b.createdAt));
+  }
+  return productsCopy;
+
+})
+
+
 const paginatedProducts= computed(()=>{
   const start =(currentPage.value-1)*perPage
   const end =start+perPage
-  return allProducts.value.slice(start,end)
+  return sortedProducts.value.slice(start,end)
 
 })
 const totalPages =computed(()=>{
-  return Math.ceil(allProducts.value.length /perPage) 
+  return Math.ceil(sortedProducts.value.length /perPage) 
 })
 function changePage(page){
   if (page >=1 && page <= totalPages.value){
